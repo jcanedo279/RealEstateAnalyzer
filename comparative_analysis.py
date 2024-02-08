61af152fd0320dff1147482ef13c9a41294cc383
jcanedo279 <52115366+jcanedo279@users.noreply.github.com>
2024-02-07 22:25:31 -0800

    Run sanity check to ensure VOO and SPY returns the correct (alpha,beta) pair. Also adjust calculation so that risk free rate is rolling. Generally clean up code.

--- comparative_analysis.py
+++ comparative_analysis.py
@@ -17,25 +17,17 @@ import sys
 def deannualize(annual_rate, periods=365):
     return (1 + annual_rate) ** (1/periods) - 1
 
-def get_risk_free_rate():
-    # Download 3-month us treasury bills rates.
-    annualized = yf.download("^IRX")["Adj Close"]
-    daily = annualized.apply(deannualize)
-    return pd.DataFrame({"annualized": annualized, "daily": daily})
+def fetch_risk_free_rate(start_date):
+    df_rf = yf.download("^IRX", start=start_date, progress=False)
+    df_rf['Risk Free Rate'] = df_rf['Adj Close'].apply(lambda x: deannualize(x/100))
+    return df_rf['Risk Free Rate']
 
 # Function to fetch historical data for a given ticker
 def fetch_stock_data_frame(ticker, starting_date):
-    df = yf.download(ticker, start=starting_date)
+    df = yf.download(ticker, start=starting_date, progress=False)
     # Ensure the datetime index is timezone-naive for compatibility.
     df.index = df.index.tz_localize(None)
     return df
-
-# Function to calculate moving averages
-def calculate_moving_averages(data, windows=[30, 90]):
-    moving_averages = {}
-    for window in windows:
-        moving_averages[f'{window}_day_MA'] = data['Adj Close'].rolling(window=window).mean()
-    return pd.DataFrame(moving_averages, index=data.index)
 
 # Function for linear interpolation of Zestimate data
 def interpolate_timeseries(data):
@@ -45,15 +37,12 @@ def interpolate_timeseries(data):
     # Resample to daily frequency filling NaN values with linear interpolation
     return data.resample('D').mean().interpolate(method='linear')
 
-
-# Main function
-def main():
+def calculate_alpha_beta_statistics():
     ticker = "SPY" # SnP 500
     # ticker = "SPG"  # Simon Property Group
     # ticker = "O" # Realty income corporations
 
-    annualized_risk_free_rate = get_risk_free_rate().iloc[-1]['daily']
-    print(annualized_risk_free_rate)
+    alpha_beta_values = []
 
     # Loop through each zip code.
     for zip_code_folder in glob.glob(os.path.join('PropertyDetails', '*')):
@@ -75,29 +64,29 @@ def main():
                 zestimate_history_df.index = pd.to_datetime(zestimate_history_df.index).normalize()
 
                 index_df = fetch_stock_data_frame(ticker, zestimate_history_df.index[0])
+                df_rf = fetch_risk_free_rate(zestimate_history_df.index[0])
 
-                # index_df = 
                 aligned_df = pd.merge_asof(zestimate_history_df, index_df, on='Date', direction='nearest')
+                aligned_df = pd.merge_asof(aligned_df, df_rf, on='Date', direction='nearest')
                 aligned_df.set_index('Date', inplace=True)
 
                 aligned_df['Home Value Returns'] = aligned_df['Home Value'].pct_change()
                 aligned_df['Stock Returns'] = aligned_df['Adj Close'].pct_change()
+                # Get rid of the first row since it does not have a percent change reference.
                 aligned_df.dropna(inplace=True)
-
-                print(aligned_df)
 
                 cov_matrix = aligned_df[['Home Value Returns', 'Stock Returns']].cov()
                 beta = cov_matrix.loc['Home Value Returns', 'Stock Returns'] / aligned_df['Stock Returns'].var()
                 
-                # Calculate Alpha using a risk free rate.
-                alpha = (aligned_df['Home Value Returns'].mean() - annualized_risk_free_rate) - beta * (aligned_df['Stock Returns'].mean() - annualized_risk_free_rate)
+                # Calculate Alpha using a risk free rate and the CAPM formula.
+                alpha = aligned_df.apply(lambda row: (row['Home Value Returns'] - row['Risk Free Rate']) - beta * (row['Stock Returns'] - row['Risk Free Rate']), axis=1).mean()
             
-                print(f"Alpha: {alpha}, Beta: {beta}")
+                alpha_beta_values.append( {"Alpha": alpha, "Beta": beta} )
 
-
-    # priceHistory
-    
-    # print(data.tail())  # Display the last few rows to see the moving averages
+    alpha_beta_df = pd.DataFrame(alpha_beta_values)
+    # Calculate aggregate statistics
+    aggregate_statistics = alpha_beta_df.describe()
+    print(aggregate_statistics)
 
 if __name__ == "__main__":
-    main()
+    calculate_alpha_beta_statistics()
